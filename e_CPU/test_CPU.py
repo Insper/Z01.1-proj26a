143fd611eba2dce023bbf2ffd070dfa04c1beed6
renan-tr <83995225+renan-tr@users.noreply.github.com>
2026-04-15 19:58:30 -0300

    Update test_CPU.py

--- e_CPU/test_CPU.py
+++ e_CPU/test_CPU.py
@@ -2,13 +2,11 @@ from cocotb_test.simulator import run
 import pytest
 import os
 
-
 def source(name):
 	dir = os.path.dirname(__file__)
 	src_dir = os.path.join(dir, 'src' )
 	return os.path.join(src_dir, name)
-
-
+	
 def test_controlunit():
     run(vhdl_sources=[source("controlunit.vhd")], 
     toplevel="controlunit", 
@@ -17,22 +15,20 @@ def test_controlunit():
     toplevel_lang="vhdl")
 
 def test_memoryio():
-    run(vhdl_sources=[source('memoryio.vhd'),source('../CPU_FPGA/Dispositivos/screen.vhd'),source('../CPU_FPGA/Dispositivos/ram16k.vhd'),source('../../b_logComb/src/mux4way16.vhd'),source('../../d_logSeq/src/register16.vhd')], 
+    run(vhdl_sources=[source('memoryio.vhd'),source('../CPU_FPGA/Dispositivos/ram16k.vhd'),source('../../b_logComb/src/mux4way16.vhd'),source('../../d_logSeq/src/register16.vhd'),source('../../d_logSeq/src/register8.vhd'),source('../../d_logSeq/src/binarydigit.vhd'),source('../../d_logSeq/src/flipflopd.vhd'),source('../../b_logComb/src/mux2way.vhd')], 
     toplevel="memoryio", 
     python_search=[os.path.join(os.path.dirname(__file__), 'test_cases')],
     module='tb_MemoryIO', 
     toplevel_lang="vhdl")
 
 def test_cpu():
-    run(vhdl_sources=[source('cpu.vhd'),source('controlunit.vhd'),source('../../b_logComb/src/mux16.vhd'),source('../../c_ULA/src/alu.vhd'),source('../../d_logSeq/src/register16.vhd'),source('../../d_logSeq/src/pc.vhd'),source('../../d_logSeq/src/register8.vhd'),source('../../d_logSeq/src/binarydigit.vhd'),source('../../d_logSeq/src/flipflopd.vhd'),source('../../b_logComb/src/mux2way.vhd'),source("../../c_ULA/src/zerador16.vhd"), source("../../c_ULA/src/inversor16.vhd"), source("../../c_ULA/src/add16.vhd"), source("../../c_ULA/src/fulladder.vhd"), source("../../b_logComb/src/and16.vhd"), source("../../c_ULA/src/comparador16.vhd")], 
+    run(vhdl_sources=[source('cpu.vhd'),source('controlunit.vhd'),source('../../b_logComb/src/mux16.vhd'),source('../../c_ULA/src/alu.vhd'),source('../../d_logSeq/src/register16.vhd'),source('../../d_logSeq/src/pc.vhd'),source('../../d_logSeq/src/register8.vhd'),source('../../d_logSeq/src/binarydigit.vhd'),source('../../d_logSeq/src/flipflopd.vhd'),source('../../b_logComb/src/mux2way.vhd'),source("../../c_ULA/src/zerador16.vhd"), source("../../c_ULA/src/inversor16.vhd"), source("../../c_ULA/src/add16.vhd"), source("../../c_ULA/src/fulladder.vhd"), source("../../b_logComb/src/and16.vhd"), source("../../c_ULA/src/comparador16.vhd"), source("../../c_ULA/src/inc16.vhd")],
     toplevel="cpu", 
     python_search=[os.path.join(os.path.dirname(__file__), 'test_cases')],
     module='tb_CPU', 
     toplevel_lang="vhdl")
 
-
 if __name__ == "__main__":
     test_controlunit()
     test_memoryio()
     test_cpu()
-
